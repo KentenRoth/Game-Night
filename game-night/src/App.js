862db6f7e3e0357e220997d6c2854ee738f20f03
Kent Roth <kentencroth@yahoo.com>
2020-03-01 08:54:37 -0600

    Implimenting react router

--- game-night/src/App.js
+++ game-night/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './Components/Login/Login';
+import SignUp from './Components/SignUp/SignUp';
 import './App.css';
 
 function App() {
 	return (
-		<div className="App">
-			<Login />
-		</div>
+		<BrowserRouter>
+			<Switch>
+				<Route path="/" component={Login} exact />
+				<Route path="/login" component={Login} />
+				<Route path="/signup" component={SignUp} />
+			</Switch>
+		</BrowserRouter>
 	);
 }
 

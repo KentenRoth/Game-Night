156daeeb4dc32bb387e8dc73b7eef58abc474dd4
Kent Roth <kentencroth@yahoo.com>
2020-03-01 14:12:04 -0600

    Redirect to game login after succesful sign up

--- game-night/src/Components/SignUp/SignUp.js
+++ game-night/src/Components/SignUp/SignUp.js
@@ -1,52 +1,76 @@
 import React from 'react';
 import axios from 'axios';
+import { Redirect } from 'react-router-dom';
 
 import LargeButton from '../Buttons/LargeButton';
 import Input from '../Inputs/Input';
 
-const SignUp = () => {
-	return (
-		<div className="content">
-			<div className="container">
-				<div className="box">
-					<form onSubmit={onSubmit}>
-						<Input type={'text'} place={'Name'} name={'name'} />
-						<Input
-							type={'text'}
-							place={'Username'}
-							name={'username'}
-						/>
-						<Input type={'text'} place={'Email'} name={'email'} />
-						<Input
-							type={'password'}
-							place={'Password'}
-							name={'password'}
-						/>
-						<LargeButton
-							color={'#0F76C0'}
-							text={'Create Account'}
-						/>
-					</form>
+class SignUp extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			redirect: false
+		};
+		this.onSubmit = this.onSubmit.bind(this);
+	}
+
+	render() {
+		const { redirect } = this.state;
+
+		if (redirect) {
+			return <Redirect to="/creategame" />;
+		}
+		return (
+			<div className="content">
+				<div className="container">
+					<div className="box">
+						<form onSubmit={this.onSubmit}>
+							<Input type={'text'} place={'Name'} name={'name'} />
+							<Input
+								type={'text'}
+								place={'Username'}
+								name={'username'}
+							/>
+							<Input
+								type={'text'}
+								place={'Email'}
+								name={'email'}
+							/>
+							<Input
+								type={'password'}
+								place={'Password'}
+								name={'password'}
+							/>
+							<LargeButton
+								color={'#0F76C0'}
+								text={'Create Account'}
+							/>
+						</form>
+					</div>
 				</div>
 			</div>
-		</div>
-	);
-};
+		);
+	}
 
-const onSubmit = e => {
-	e.preventDefault();
-	const name = e.target.name.value;
-	const username = e.target.username.value;
-	const email = e.target.email.value;
-	const password = e.target.password.value;
-	axios
-		.post('./user', {
-			name,
-			username,
-			email,
-			password
-		})
-		.then(res => console.log(res));
-};
+	onSubmit(e) {
+		e.preventDefault();
+		const name = e.target.name.value;
+		const username = e.target.username.value;
+		const email = e.target.email.value;
+		const password = e.target.password.value;
+		axios
+			.post('./user', {
+				name,
+				username,
+				email,
+				password
+			})
+			.then(res => {
+				if (res.status === 201) {
+					this.setState({ redirect: true });
+				}
+			});
+	}
+}
 
 export default SignUp;

5f4918c39a03ec8e24600245165f4b55378a951f
Kent Roth <kentencroth@yahoo.com>
2020-03-07 15:23:57 -0600

    Fixed issue with config

--- game-night/src/Components/CreatePlayer/CreatePlayer.js
+++ game-night/src/Components/CreatePlayer/CreatePlayer.js
@@ -1,61 +1,73 @@
 import React from 'react';
 import axios from 'axios';
+import { Redirect, Link } from 'react-router-dom';
 
 import LargeButton from '../Buttons/LargeButton';
 import Input from '../Inputs/Input';
 
-const CreatePlayer = () => {
-	return (
-		<div className="content">
-			<div className="container">
-				<div className="box">
-					<form onSubmit={onSubmit}>
-						<Input
-							type={'text'}
-							place={'Player Name'}
-							name={'name'}
-						/>
+class CreatePlayer extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			redirect: false,
+			token: localStorage.getItem('gameAuthToken')
+		};
+		this.onSubmit = this.onSubmit.bind(this);
+	}
+	render() {
+		const { redirect } = this.state;
+		if (redirect) {
+			//Page not created yet
+			//return <Redirect to="/InGame" />
+		}
+		return (
+			<div className="content">
+				<div className="container">
+					<div className="box">
+						<form onSubmit={this.onSubmit}>
+							<Input
+								type={'text'}
+								place={'Player Name'}
+								name={'name'}
+							/>
 
-						<Input
-							type={'password'}
-							place={'Pin Number'}
-							name={'pin'}
-						/>
+							<Input
+								type={'password'}
+								place={'Pin Number'}
+								name={'pin'}
+							/>
 
-						<LargeButton
-							type={'submit'}
-							color={'#88368D'}
-							text={'Create Player'}
-						/>
-					</form>
+							<LargeButton
+								type={'submit'}
+								color={'#88368D'}
+								text={'Create Player'}
+							/>
+						</form>
+					</div>
 				</div>
 			</div>
-		</div>
-	);
-};
-
-const token = localStorage.getItem('gameAuthToken');
-
-const config = {
-	headers: {
-		Authorization: 'Bearer ' + token
+		);
 	}
-};
 
-// Need to setup the config to grab the authToken from when game is created.
-const onSubmit = e => {
-	const name = e.target.name.value;
-	const pin = e.target.pin.value;
-	e.preventDefault();
+	onSubmit(e) {
+		const name = e.target.name.value;
+		const pin = e.target.pin.value;
+		const config = {
+			headers: {
+				Authorization: 'Bearer ' + this.token
+			}
+		};
+		e.preventDefault();
 
-	axios.post(
-		'/ingameuser',
-		{
-			name,
-			pin
-		},
-		config
-	);
-};
+		axios.post(
+			'/ingameuser',
+			{
+				name,
+				pin
+			},
+			config
+		);
+	}
+}
 
 export default CreatePlayer;

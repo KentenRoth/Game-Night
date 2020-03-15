4a289db3ed91b9b9483d3aa0855cdfe7b3a5ea96
Kent Roth <kentencroth@yahoo.com>
2020-03-15 10:32:10 -0500

    Setting up property Card

---
+++ game-night/src/Components/PlayerInGame/PropertyCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import SmallButton from '../Buttons/SmallButton';
+
+class PropertyCard extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div>
+				<div></div>
+			</div>
+		);
+	}
+}
+
+export default PropertyCard;

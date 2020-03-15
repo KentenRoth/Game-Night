a3e502c4d8ea9c2747e6f1b56f3681175d163bf9
Kent Roth <kentencroth@yahoo.com>
2020-03-15 12:54:38 -0500

    Adjusted to account for not buying house

--- game-night/src/Components/Buttons/MortgageButton.js
+++ game-night/src/Components/Buttons/MortgageButton.js
@@ -1,28 +1,52 @@
 import React from 'react';
 
 const MortgageButton = props => {
-	const color = props.color;
+	const isButtonDisabled = () => {
+		if (props.text === 'Buy House' && props.disabled === true) {
+			return {
+				backgroundColor: props.color + '7F',
+				border: 'none',
+				borderRadius: '20px',
+				color: 'white',
+				cursor: 'not-allowed',
+				fontSize: '12px',
+				fontWeight: 'bold',
+				letterSpacing: '0.1em',
+				margin: '10px auto',
+				padding: '2px',
+				width: '150px'
+			};
+		} else {
+			return {
+				backgroundColor: props.color,
+				border: 'none',
+				borderRadius: '20px',
+				color: 'white',
+				fontSize: '12px',
+				fontWeight: 'bold',
+				letterSpacing: '0.1em',
+				margin: '10px auto',
+				padding: '2px',
+				width: '150px'
+			};
+		}
+	};
 
-	const buttonStyle = {
-		backgroundColor: color,
-		border: 'none',
-		borderRadius: '20px',
-		color: 'white',
-		fontSize: '12px',
-		fontWeight: 'bold',
-		letterSpacing: '0.1em',
-		margin: '10px auto',
-		padding: '2px',
-		width: '150px'
+	const disabledButton = () => {
+		if (props.text === 'Buy House' && props.disabled === true) {
+			return 'disabled';
+		}
+		return;
 	};
 
 	return (
 		<button
 			type={props.type}
-			style={buttonStyle}
+			style={isButtonDisabled()}
 			name={props.name}
 			token={props.token}
 			gameid={props.gameid}
+			disabled={disabledButton()}
 		>
 			{props.text}
 		</button>

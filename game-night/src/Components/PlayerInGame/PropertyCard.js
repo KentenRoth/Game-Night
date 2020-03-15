9f8e4e2b1dc9d95b3c042b68e05993df2b591dd4
Kent Roth <kentencroth@yahoo.com>
2020-03-15 12:55:30 -0500

    Built out property card

--- game-night/src/Components/PlayerInGame/PropertyCard.js
+++ game-night/src/Components/PlayerInGame/PropertyCard.js
@@ -1,16 +1,56 @@
 import React from 'react';
 
-import SmallButton from '../Buttons/SmallButton';
+import MortgageButton from '../Buttons/MortgageButton';
 
 class PropertyCard extends React.Component {
-	constructor(props) {
-		super(props);
-	}
+	card = {
+		backgroundColor: '#777777',
+		borderRadius: '5px',
+		border: '1px solid black',
+		padding: '10px',
+		margin: '10px',
+		width: '170px'
+	};
+
+	colorBox = {
+		backgroundColor: this.props.property.Color,
+		border: '1px solid black',
+		height: '50px',
+		margin: 'auto',
+		width: '150px'
+	};
 
 	render() {
 		return (
-			<div>
-				<div></div>
+			<div className="box">
+				<div style={this.card}>
+					<div style={this.colorBox}></div>
+					<div>
+						<p>{this.props.property.Deed}</p>
+					</div>
+					<hr />
+					<div>
+						<p>Rent: ${this.props.property.Rent}</p>
+					</div>
+					<div>
+						<p>House Cost: ${this.props.property.HouseCost}</p>
+					</div>
+					<div>
+						{/* This will need function */}
+						<p># of Houses: 0</p>
+					</div>
+					<div>
+						{/* This will need function to figure out */}
+						<p>Hotel: No</p>
+					</div>
+					<hr />
+					<MortgageButton
+						text={'Buy House'}
+						color={'#18AC4C'}
+						disabled={true}
+					/>
+					<MortgageButton text={'Mortgage'} color={'#E73B27'} />
+				</div>
 			</div>
 		);
 	}

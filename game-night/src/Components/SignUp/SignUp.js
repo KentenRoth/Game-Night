c969bf07e4940e9724be1496b0dcbf5f80b98424
Kent Roth <kentencroth@yahoo.com>
2020-03-20 15:37:32 -0500

    Error handling and displaying errors

--- game-night/src/Components/SignUp/SignUp.js
+++ game-night/src/Components/SignUp/SignUp.js
@@ -5,6 +5,31 @@ import { Redirect } from 'react-router-dom';
 import LargeButton from '../Buttons/LargeButton';
 import Input from '../Inputs/Input';
 
+function checkInputsBeforeSubmit(name, username, email, password, password2) {
+	const isBetween = (num, min, max) => {
+		return num >= min && num <= max;
+	};
+
+	let errors = [];
+	const emailRegex = new RegExp(
+		/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
+	);
+	const passwordRegex = new RegExp(
+		'^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{5,15}$'
+	);
+	if (isBetween(name.length, 2, 20) === false) errors.push('name');
+
+	if (isBetween(username.length, 2, 20) === false) errors.push('username');
+
+	if (emailRegex.test(email) === false) errors.push('email');
+
+	if (passwordRegex.test(password) === false) errors.push('password');
+
+	if (password !== password2) errors.push('password2');
+
+	return errors;
+}
+
 class SignUp extends React.Component {
 	constructor(props) {
 		super(props);
@@ -17,7 +42,7 @@ class SignUp extends React.Component {
 	}
 
 	render() {
-		const { redirect } = this.state;
+		const { redirect, errors } = this.state;
 
 		if (redirect) {
 			return <Redirect to="/creategame" />;
@@ -26,6 +51,9 @@ class SignUp extends React.Component {
 			<div className="content">
 				<div className="container">
 					<div className="box">
+						<div>
+							{errors.map(error => this.errorDisplay(error))}
+						</div>
 						<div>{this.inputDisplay()}</div>
 						<form onSubmit={this.onSubmit}>
 							<Input
@@ -75,7 +103,7 @@ class SignUp extends React.Component {
 	}
 
 	whatInput = e => {
-		return this.setState({ selectedInput: e.target.name, errors: [] });
+		return this.setState({ selectedInput: e.target.name });
 	};
 
 	clearInput = () => {
@@ -91,19 +119,19 @@ class SignUp extends React.Component {
 						<p style={this.bullet}>2-20 characters</p>
 					</div>
 				);
-				break;
+
 			case 'username':
 				return (
 					<div>
-						<div styel={this.title}></div>
-						Username
+						<div style={this.title}>Username</div>
+
 						<p style={this.bullet}>2-20 characters</p>
 					</div>
 				);
-				break;
+
 			case 'email':
 				return <div style={this.title}>Email Address</div>;
-				break;
+
 			case 'password':
 				return (
 					<div>
@@ -113,7 +141,7 @@ class SignUp extends React.Component {
 						<p style={this.bullet}>1 number</p>
 					</div>
 				);
-				break;
+
 			case 'password2':
 				return (
 					<div>
@@ -121,11 +149,58 @@ class SignUp extends React.Component {
 						<p style={this.bullet}>Passwords must match</p>
 					</div>
 				);
+			default:
+				return;
 		}
 	};
 
-	passwordCheck = () => {
-		// ^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{5,15}$
+	errorDisplay = error => {
+		switch (error) {
+			case 'name':
+				return (
+					<div key={error} style={this.errorTags}>
+						<div style={this.titleError}>Name</div>
+						<p style={this.bulletError}>2-20 characters</p>
+					</div>
+				);
+
+			case 'username':
+				return (
+					<div key={error} style={this.errorTags}>
+						<div style={this.titleError}>Username</div>
+
+						<p style={this.bulletError}>2-20 characters</p>
+					</div>
+				);
+
+			case 'email':
+				return (
+					<div key={error} style={this.errorTags}>
+						<div style={this.titleError}>Email Address</div>
+					</div>
+				);
+
+			case 'password':
+				return (
+					<div key={error} style={this.errorTags}>
+						<div style={this.titleError}>Password</div>
+						<p style={this.bulletError}>5-15 characters</p>
+						<p style={this.bulletError}>1 letter</p>
+						<p style={this.bulletError}>1 number</p>
+					</div>
+				);
+
+			case 'password2':
+				return (
+					<div key={error} style={this.errorTags}>
+						<div style={this.titleError}>
+							Passwords Do Not Match
+						</div>
+					</div>
+				);
+			default:
+				return;
+		}
 	};
 
 	onSubmit(e) {
@@ -134,20 +209,34 @@ class SignUp extends React.Component {
 		const username = e.target.username.value;
 		const email = e.target.email.value;
 		const password = e.target.password.value;
-		axios
-			.post('./user', {
-				name,
-				username,
-				email,
-				password
-			})
-			.then(res => {
-				if (res.status === 201) {
-					localStorage.setItem('userAuthToken', res.data.authToken);
-					localStorage.setItem('userID', res.data.user._id);
-					this.setState({ redirect: true });
-				}
-			});
+		const password2 = e.target.password2.value;
+		const errors = checkInputsBeforeSubmit(
+			name,
+			username,
+			email,
+			password,
+			password2
+		);
+
+		if (errors.length > 0) {
+			this.setState({ errors });
+			return;
+		}
+
+		// axios
+		// 	.post('./user', {
+		// 		name,
+		// 		username,
+		// 		email,
+		// 		password
+		// 	})
+		// 	.then(res => {
+		// 		if (res.status === 201) {
+		// 			localStorage.setItem('userAuthToken', res.data.authToken);
+		// 			localStorage.setItem('userID', res.data.user._id);
+		// 			this.setState({ redirect: true });
+		// 		}
+		// 	});
 	}
 
 	title = {
@@ -155,6 +244,20 @@ class SignUp extends React.Component {
 	};
 
 	bullet = {
+		fontSize: '15px'
+	};
+
+	errorTags = {
+		margin: '10px'
+	};
+
+	titleError = {
+		color: 'red',
+		fontSize: '20px'
+	};
+
+	bulletError = {
+		color: 'red',
 		fontSize: '15px'
 	};
 }

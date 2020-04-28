685ea3c714076815917f47b41a0e2be0c563cb37
Kent Roth <kentencroth@yahoo.com>
2020-04-28 13:58:19 -0500

    Getting amount to multiply dice by

--- game-night/src/Components/PlayerInGame/ActionSection/PayUtilities.js
+++ game-night/src/Components/PlayerInGame/ActionSection/PayUtilities.js
@@ -4,15 +4,16 @@ import SmallButton from '../../Buttons/SmallButton';
 class PayUtilities extends React.Component {
 	whoHasUtilites = () => {
 		const utilitiesOwned = [];
-		this.props.utilities.map(player => {
-			player.property.map(property => {
+		this.props.utilities.map((player) => {
+			player.property.map((property) => {
 				if (
 					property.Deed === 'Electric Company' ||
 					property.Deed === 'Water Works'
 				) {
 					utilitiesOwned.push({
 						utility: property.Deed,
-						player: player._id
+						player: player._id,
+						multiplyBy: property.Rent,
 					});
 				}
 				return [];
@@ -20,17 +21,24 @@ class PayUtilities extends React.Component {
 			return [];
 		});
 
-		return utilitiesOwned.map(property => (
-			<option key={property.player} value={property.player}>
+		return utilitiesOwned.map((property) => (
+			<option
+				key={property.utility}
+				value={property.player}
+				name={property.multiplyBy}
+			>
 				{property.utility}
 			</option>
 		));
 	};
 
-	payUtilityBill = e => {
+	payUtilityBill = (e) => {
 		let pay = {
 			userID: this.refs.utility.value,
-			diceAmount: e.target.amount.value
+			diceAmount: e.target.amount.value,
+			multiplyBy: parseInt(
+				this.refs.utility.selectedOptions[0].attributes[1].value
+			),
 		};
 		e.preventDefault();
 		this.props.payUtilities(pay);
